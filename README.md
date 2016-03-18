daf35c563f93227370b242b18372d0047e0b91a4
zhouhuashan <zhouhuashan@91yong.com>
2016-03-18 19:52:28 +0800

    init project

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # gcmd
-visual studio code plugin
+Launch a new command line that target the current document path and contains the custom environment variables
+
+# Commands
+* ctrl+shift+p then gcmd:run
+
+# Settings
+By default, this extension use the native command line in your OS. But, you can override this behaviour by setting a different executable in the settings.json of your workspace. Here is an example if we prefer use custom environment variables :
+
+// Place your settings in this file to overwrite default and user settings.
+{
+    "gcmd.env": {
+        "GOPATH": "{go.gopath};{GOPATH}",
+        "GOBIN":"d:\\golang\\bin"
+    }
+}
+
+There are two reserved variables.They mean what their literal mean
+
+* {currentpath}
+* {rootpath}
---
+++ src/setting.js
@@ -0,0 +1,82 @@
+'use strict';
+var vscode = require('vscode');
+var path = require('path')
+
+// getCommandLine 获取配置命令行信息
+function getCommandLine() {
+    var configuration = vscode.workspace.getConfiguration("gcmd");
+    var envStr = 'set TEST=123';
+    var parten = /\{(.+?)\}/g;
+    var genv = configuration.get("env");
+    if (genv) {
+        for (var item in genv) {                        
+            if (parten.test(genv[item])) {
+                envStr += " & set " + item + "=" + genv[item].replace(parten, replaceVar);
+            } else {
+                envStr += " & set " + item + "=" + genv[item];
+            }
+        }
+    }
+    var envTrimStr = envStr.trim();
+    if(envTrimStr.length > 0 && envTrimStr[0] == '&') envStr = envTrimStr.substring(1);
+    
+    var pathName = currentPath();
+    var cmd = envStr + ' & start cmd /k cd /D "' + pathName + '"';
+    return cmd;
+}
+exports.getCommandLine = getCommandLine
+
+// replaceVar 替换环境变量
+function replaceVar(whole, key) {
+    console.log(key)
+    if (!key || key.length == 0) return "";
+    key = key.trim();
+    switch (key) {
+        case "currentpath":
+            return currentPath()
+        case "rootpath":
+            return rootPath()
+    }
+    var val = getConfigByOriginalKey(key);
+    if (val) return val;
+
+    val = getProcessEnv(key)
+    if (val) return val;
+
+    return key;
+}
+
+// currentPath 获取当前是文件的目录
+function currentPath() {
+    return path.dirname(vscode.window.activeTextEditor.document.fileName);
+}
+
+// rootPath 获取当前是文件的目录
+function rootPath() {
+    return vscode.workspace.rootPath
+}
+
+// getConfigByOriginalKey 获取当前工作目录下的配置,若无配置，则传回undefine;
+// originalKey 配置文件中的Key值，如：gcmd.go.gopath
+function getConfigByOriginalKey(originalKey) {
+    if (originalKey.indexOf('.') <= 0) {
+        return vscode.workspace.getConfiguration("").get(originalKey);
+    }
+
+    var subKeys = originalKey.split('.');
+    var config = vscode.workspace.getConfiguration(subKeys[0]);
+    if (!config) return config;
+
+    var valObj = config.get(subKeys[1])
+    if (!valObj) return valObj;
+
+    for (var i = 2; i < subKeys.length; i++) {
+        valObj = valObj[subKeys[i]];
+    }
+    return valObj;
+}
+
+// getProcessEnv获取环境变量的值
+function getProcessEnv(originalKey) {
+    return process.env[originalKey];
+}
